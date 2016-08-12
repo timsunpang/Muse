dfa6ed05a316bd776bcdb6d44993d744cb1e6904
timsunpang <timsunpang@gmail.com>
2016-08-11 22:29:11 -0700

    added controllers, edited user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,45 @@
 #
 
 class User < ActiveRecord::Base
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize: :ensure_session_token
+
+  has_many :comments
+  has_many :likes
+  has_many :playlists
+  has_many :tracks
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_token!
+    self.session_token = self.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.generate_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'static_pages#main'
 
-
+  namespace :api do
+    resource :session, only: [:new, :create, :destroy]
+    resources :users, only: [:new, :update, :create, :show, :destroy]
+    resources :tracks, only: [:index, :update, :create, :show, :destroy]
+    resources :playlists, only: [:index, :update, :create, :show, :destroy]
+    resources :playlistings, only: [:index, :create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
+    resources :comments, only: [:index, :update, :create, :show, :destroy]
+  end
 end
