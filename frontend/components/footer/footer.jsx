c5c747b9d34868afb228361a3fb25fcf7c45c75f
JoshuaHwang <ijoshjosh@gmail.com>
2016-08-26 11:24:53 -0700

    create footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,9 +1,26 @@
 const React = require('react');
+const Footer = require("react-bootstrap").Footer;
+const Navbar = require("react-bootstrap").Navbar;
+const Nav = require("react-bootstrap").Nav;
+const NavItem = require("react-bootstrap").NavItem;
 
 module.exports = React.createClass({
-  render: function(){
-      return(
-        <div>Footer</div>
-      )
+  render: function() {
+
+    return (
+      <div>
+        <Navbar fixedBottom inverse>
+          <Nav>
+
+            <NavItem href="https://github.com/timsunpang/Muse">GitHub</NavItem>
+            <NavItem href="http://www.joshua-hwang.com">Josh</NavItem>
+            <NavItem href="http://www.rodrigocardenas.xyz">Rodrigo</NavItem>
+            <NavItem href="https://github.com/timsunpang">Tim</NavItem>
+
+
+          </Nav>
+        </Navbar>
+      </div>
+    );
   }
 });
